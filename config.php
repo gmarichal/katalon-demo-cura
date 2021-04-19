d4c0a1a974dee99c88031a83c5b6eb0ae920a42d
Gastón Marichal <gmarichal@genexusconsulting.com>
2021-04-19 11:04:58 -0300

    Update config.php

--- config.php
+++ config.php
@@ -18,10 +18,10 @@ define('TPL_DIR', __DIR__ . '/views');
 
 // Site config
 // define('SITE_URL', "http://localhost/katalon-sample/"); // website URL with ended slash
-define('SITE_URL', "https://katalon-demo-cura.herokuapp.com/");
+define('SITE_URL', "https://healthcare-demo-katalon.herokuapp.com/");
 define('SITE_NAME', "CURA Healthcare Service");
-define('SITE_ADDRESS', "Atlanta 550 Pharr Road NE Suite 525<br>Atlanta, GA 30305");
-define('SITE_TEL', "(678) 813-1KMS");
+define('SITE_ADDRESS', "Montevideo, URUGUAY");
+define('SITE_TEL', "(598) 99 123 456");
 define('SITE_EMAIL', "info@katalon.com");
 define('FOOTER_COPYRIGHT', "Copyright &copy; " . SITE_NAME . " " . date("Y"));
 
